9b28a6d27d2e1327d0881aa692e84aeea6673437
Sev <82528808+iarum@users.noreply.github.com>
2023-05-22 14:50:26 +0400

    Featured added

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,7 @@ let gameStarted = false;
 
 let car = $('.car');
 let score = 0;
+let timeout = 8000;
 
 const bgAudio = new Audio('assets/sounds/bg.mp3');
 const coin = new Audio('assets/sounds/coin.mp3');
@@ -13,14 +14,21 @@ function randomNumber(from, to) {
     return Math.floor((Math.random() * to) + from);
 }
 
+function speedUp() {
+    $("body").get(0).style.setProperty("--speed", "5s");
+}
+
+setTimeout(function () {
+
+}, 10000)
+
 $(document).on('keypress', function (e) {
     let key = e.which;
 
-    if (key == 13) {
-        $('.start').detach();
-        gameStarted = true;
-        bgAudio.play();
-    }
+    $('.start').detach();
+    gameStarted = true;
+    bgAudio.play();
+
     // a 97, d 100, w 119, s 115
     if (gameStarted) {
         if (key == 119) {
@@ -30,7 +38,7 @@ $(document).on('keypress', function (e) {
             car.css('top', "20%");
         }
         if (key == 100) {
-            car.css('left', car.offset().left + 40 + 'px');
+            car.css('left', car.offset().left + 50 + 'px');
         }
         if (key == 97) {
             car.css('left', car.offset().left - 40 + 'px');
@@ -45,7 +53,7 @@ setInterval(function () {
     $('.clouds').append(`<i class="fa-solid fa-cloud cloud" style="top: ${randomNumber(0, height * 0.75)}px; left: ${width}px;"></i>`);
     setTimeout(function () {
         $('.clouds').find('.cloud:first').detach();
-    }, 15000)
+    }, 30000)
 }, 2000)
 
 // 🚗
@@ -53,7 +61,7 @@ setInterval(function () {
     $('.lines').append(`<div class="line" style="left: ${width}px;"></div>`);
     setTimeout(function () {
         $('.lines').find('.line:first').detach();
-    }, 15000)
+    }, timeout * 2)
 }, 400)
 
 // 🌲
@@ -61,14 +69,14 @@ setInterval(function () {
     $('.trees').append('<img src="assets/img/tree.png" class="tree" style="left:' + width + 'px; "/>');
     setTimeout(function () {
         $('.trees').find('.tree:first').detach();
-    }, 8000)
+    }, timeout)
 }, 5000)
 
 setInterval(function () {
     $('.grass').append('<img src="assets/img/grass.png" class="gras" style="left:' + width + 'px; "/>');
     setTimeout(function () {
         $('.grass').find('.gras:first').detach();
-    }, 8000)
+    }, timeout)
 }, 4500)
 
 // 🐛
@@ -79,9 +87,20 @@ setInterval(function () {
         $('.worms').append(`<img class="worm" src="assets/img/hole.png" style="top: ${currentPosition}; left: ${width}px;"/>`);
         setTimeout(function () {
             $('.worms').find('.worm:first').detach();
-        }, 8000)
+        }, timeout)
     }
 }, 2100)
+
+setInterval(function () {
+    if (gameStarted) {
+        let position = ['-30px', '45px']
+        let currentPosition = position[Math.floor(Math.random() * position.length)];
+        $('.worms').append(`<img class="taxi" src="assets/img/taxi.png" style="top: ${currentPosition}; left: ${width}px;"/>`);
+        setTimeout(function () {
+            $('.worms').find('.taxi:first').detach();
+        }, timeout)
+    }
+}, 20000)
 
 // 💲
 setInterval(function () {
@@ -92,18 +111,42 @@ setInterval(function () {
     }
 }, 3500)
 
+setInterval(function () {
+    if (gameStarted) {
+        let position = ['-10px', '60px']
+        let currentPosition = position[Math.floor(Math.random() * position.length)];
+        $('.coins').append(`<img class="more-coin" src="assets/img/money.png" style="top: ${currentPosition}; left: ${width}px;"/>`);
+    }
+}, 15000)
+
 // Win or die
 setInterval(function () {
     if (gameStarted) {
         $('.worm').each(function () {
             if (
                 (car.offset().left - $(this).offset().left) < 50 &&
-                (car.offset().left - $(this).offset().left) > - 130 &&
-                (car.offset().top - $(this).offset().top) < 30 &&
+                (car.offset().left - $(this).offset().left) > - 120 &&
+                (car.offset().top - $(this).offset().top) < 0 &&
                 (car.offset().top - $(this).offset().top) > - 70
             ) {
                 $('.car').detach();
                 death.play();
+                bgAudio.pause();
+                $('.end').find('span').text(score);
+                $('.end').show();
+            }
+        })
+
+        $('.taxi').each(function () {
+            if (
+                (car.offset().left - $(this).offset().left) < 50 &&
+                (car.offset().left - $(this).offset().left) > - 120 &&
+                (car.offset().top - $(this).offset().top) < 0 &&
+                (car.offset().top - $(this).offset().top) > - 70
+            ) {
+                $('.car').detach();
+                death.play();
+                bgAudio.pause();
                 $('.end').find('span').text(score);
                 $('.end').show();
             }
@@ -117,6 +160,21 @@ setInterval(function () {
                 (car.offset().top - $(this).offset().top) > - 70
             ) {
                 score++;
+                coin.play();
+                $(this).detach();
+                $('.score').find('span').text(score);
+
+            }
+        })
+
+        $('.more-coin').each(function () {
+            if (
+                (car.offset().left - $(this).offset().left) < 50 &&
+                (car.offset().left - $(this).offset().left) > - 130 &&
+                (car.offset().top - $(this).offset().top) < 10 &&
+                (car.offset().top - $(this).offset().top) > - 70
+            ) {
+                score = score + 5;
                 coin.play();
                 $(this).detach();
                 $('.score').find('span').text(score);
